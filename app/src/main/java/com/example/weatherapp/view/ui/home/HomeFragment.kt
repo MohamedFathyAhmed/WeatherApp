abc02fab9d906368c6813fdc45ec6ee1c17cc2be
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-02-23 02:32:46 +0200

    add : gps

--- app/src/main/java/com/example/weatherapp/view/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/ui/home/HomeFragment.kt
@@ -1,11 +1,23 @@
 package com.example.weatherapp.view.ui.home
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,12 +32,15 @@ import com.example.weatherapp.databinding.FragmentHomeBinding
 import com.example.weatherapp.getWeatherIcon
 import com.example.weatherapp.model.TimeState
 import com.example.weatherapp.model.Weather
-
+import com.google.android.gms.location.*
+import android.content.Context.LOCATION_SERVICE as ContextLOCATION_SERVICE
+const val PERMISSION_ID = 44
 class HomeFragment : Fragment() {
     lateinit var viewModel: HomeDataViewModel
+    var latitude: Double =0.0
+    var longitude: Double =0.0
     private lateinit var _binding: FragmentHomeBinding
-  //  private lateinit var daysAdapter: DaysAdapter
-    //private lateinit var timesAdapter: TimesAdapter
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
     private val binding get() = _binding
 
     @SuppressLint("SetTextI18n")
@@ -34,6 +49,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this, DataViewModelFactory(requireContext())).get(HomeDataViewModel::class.java)
 
@@ -76,6 +92,7 @@ class HomeFragment : Fragment() {
         }
 
 
+        getLastLocation()
 
         return binding.root
     }
@@ -102,5 +119,89 @@ class HomeFragment : Fragment() {
     private fun setWeatherIcon(weather: Weather) {
     //   _binding.imgIcon.setImageResource(getWeatherIcon(weather.state, currentTimeState!!))
     }
+    private fun checkPermissions(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf<String>(
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+            ), PERMISSION_ID
+        )
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                requestNewLocationData()
+
+            }else{
+                val intent = Intent (Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity (intent)
+
+            }
+        } else {
+            requestPermissions()
+
+        }
+    }
+
+    fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+
+
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        mLocationRequest.setInterval(0)
+         mLocationRequest.setFastestInterval(0);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            latitude  = mLastLocation. latitude
+            longitude = mLastLocation. longitude
+  
+        }
+    }
 }
